04d92d63882c55730f175bc156d43133ee27b4e9
koh97222 <koh97222@gmail.com>
2020-09-14 21:05:14 +0900

    #1_トランザクションを実装

--- app/repository/login_repository.go
+++ app/repository/login_repository.go
@@ -3,27 +3,30 @@ package repository
 import (
 	"app/domain"
 	"app/infrastructure"
+	"database/sql"
 )
 
 // GetUserInfo ユーザ情報の取得
 func GetUserInfo(userID string) (domain.User, error) {
 
+	db := infrastructure.DbConnect()
+
 	var user domain.User
 
-	db := infrastructure.DbConnect()
-	query := "SELECT * FROM users WHERE userID = ? "
-
-	res, err := db.Query(query, userID)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for res.Next() {
-		err := res.Scan(&user.ID, &user.UserID, &user.Password, &user.Name, &user.Email, &user.Year, &user.Month, &user.Day, &user.Sex)
+	err := infrastructure.Transact(db, func(tx *sql.Tx) error {
+		query := "SELECT * FROM users WHERE userID = ? "
+		res, err := db.Query(query, userID)
 		if err != nil {
 			panic(err.Error())
 		}
-	}
+		for res.Next() {
+			err := res.Scan(&user.ID, &user.UserID, &user.Password, &user.Name, &user.Email, &user.Year, &user.Month, &user.Day, &user.Sex)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+		return err
+	})
 	return user, err
 }
 
@@ -31,21 +34,26 @@ func GetUserInfo(userID string) (domain.User, error) {
 func VerifyInput(u *domain.UserLoginInput) (bool, error) {
 
 	db := infrastructure.DbConnect()
-	query := "SELECT count(*) FROM users WHERE userID = ? and password = ?"
-
-	cnt, err := db.Query(query, u.UserID, u.Password)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
 
 	// userID,password照合結果確認
 	var Count domain.Cnt
-	for cnt.Next() {
-		err := cnt.Scan(&Count.Count)
+
+	err := infrastructure.Transact(db, func(tx *sql.Tx) error {
+		query := "SELECT count(*) FROM users WHERE userID = ? and password = ?"
+		cnt, err := db.Query(query, u.UserID, u.Password)
 		if err != nil {
 			panic(err.Error())
 		}
-	}
+		defer db.Close()
+
+		for cnt.Next() {
+			err := cnt.Scan(&Count.Count)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+		return err
+	})
+
 	return Count.Count != 0, err
 }
